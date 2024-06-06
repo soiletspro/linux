5f9cce20782f6d30a5876c672f7a390c3d7faace
soiletspro <stelios.tzampazidis@gmail.com>
2024-06-06 18:30:05 +0300

    test

---
+++ install-ssh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install wget curl zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+cp .zshrc ~/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+source ~/.zshrc
