7019dfaccf5e90c5de1a071bfa330c758f86957c
soiletspro <stelios.tzampazidis@gmail.com>
2024-06-06 18:22:33 +0300

    test

--- zsh-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'Insalling Docker Completion for oz my zsh'
-mkdir -p ~/.oh-my-zsh/plugins/docker/
-curl -fLo ~/.oh-my-zsh/plugins/docker/_docker https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker
-echo 'Just add docker to plugins in .zshrc'
